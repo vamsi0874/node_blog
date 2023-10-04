e3b26a8976b18afa6fb39ab9226c279feafcbb9b
vamsi0874 <vamsim0874>
2023-10-05 01:00:43 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,14 @@
+// const http = require('http');
+// const _ = require("lodash");
+
+// const server = http.createServer((req , res)=>{
+//     const num = _.random(10,20);
+//       console.log(num)
+
+//     res.setHeader('Content-Type','text/plain');
+//     res.write('vamsi ');
+//     res.end();
+// })
+// server.listen(3000,'localhost',()=>{
+//  console.log('server');
+// })
